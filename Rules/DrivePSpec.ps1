f40855fb011d7a3bf97b8062354bad8b9e3af719
christopherbauer <chrisbauer.career@gmail.com>
2015-10-11 00:13:53 -0400

    Moved rules functions out and added powershell rule Moved the functions under a rules folder then updated the examplespec to reflect new locations. Added powershell version rules.

---
+++ Rules/DrivePSpec.ps1
@@ -0,0 +1,16 @@
+﻿
+function DirectoryExists($path) {
+    @{
+        Name = $MyInvocation.MyCommand;
+        Info = $path
+        Result = Test-Path $path
+    }
+}
+
+function FileExists($pathToFile) {
+    @{
+        Name = $MyInvocation.MyCommand;
+        Info = $pathToFile
+        Result = Test-Path $pathToFile
+    }
+}
